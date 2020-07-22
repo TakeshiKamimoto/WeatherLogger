e7a3a0d6293a0bf7ca44045d180093a44f76a166
TakeshiKamimoto <68544257+TakeshiKamimoto@users.noreply.github.com>
2020-07-22 23:27:27 +0900

    Update measuring.py

--- measuring.py
+++ measuring.py
@@ -1,8 +1,8 @@
 #coding: utf-8
 
-import bme280_temp
-import bme280_pres
-import bme280_humid
+import bme280_sample
+import weathernewsdata
+import webpressdata
 import datetime
 import os
 import pandas as pd
@@ -12,8 +12,6 @@ import requests
 from bs4 import BeautifulSoup
 
 dir_path = '/home/pi/bme280/data'
-URL = 'https://weathernews.jp/onebox/tenki/tokyo/13108/'
-URL2 = 'http://web.sugiyama-u.ac.jp/~yamane/honkoma/Vantage_Pro_Plus.htm'
 
 now = datetime.datetime.now()
 filename = 'todaydata'
@@ -21,26 +19,16 @@ filename = 'todaydata'
 #date = now.strftime('%Y/%m/%d %H:%M')
 time = now.strftime('%H:%M')
 
-bme_t = bme280_temp.readData()
-bme_p = bme280_pres.readData()
-bme_h = bme280_humid.readData()
+sensdata = bme280_sample.readData()
+bme_t = sensdata[0]
+bme_p = sensdata[1]
+bme_h = sensdata[2]
 
+webdata = weathernewsdata.readData()
+temp_m = webdata[0]
+humd_m = webdata[1]
 
-r = requests.get(URL)
-html = r.text.encode(r.encoding)
-s = BeautifulSoup(html, 'lxml')
-weathernow = s.find('ul', class_='weather-now__ul')
-measdata = weathernow.find_all('li')
-temp_m = re.sub("?",'',measdata[1].contents[1])
-humd_m = re.sub('%','',measdata[2].contents[1])
-#pres_m = re.sub('hPa','',measdata[3].contents[1])
-
-r = requests.get(URL2)
-html = r.text.encode(r.encoding)
-s = BeautifulSoup(html, 'lxml')
-weathernow = s.find_all('tr')
-pres_m = re.sub('hPa','',weathernow[5].select('small')[2].text)
-
+pres_m = webpressdata.readData()
 
 if not os.path.exists('/home/pi/bme280/data'):
     os.makedirs('/home/pi/bme280/data')
